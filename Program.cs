78d2b6446c965e8bfc0ca192d5c6f8fe7bbb774b
Dylan <nichodo@g.toiohomai.ac.nz>
2017-10-24 10:40:30 +1300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _9994086_assessment3_question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Start the program with Clear();
+            Console.Clear();
+
+            var falconers = new string[6] {"Debbie", "Heidi", "Noel", "Melissa", "Kirsten", "Koshy"};
+            var falcons = new string[6] {"Hisan", "Ka Kite", "Siren", "Koa", "Stella", "Mojo"};
+
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Flying for the Wingspan Display at 2pm today we have:\n");
+            Console.WriteLine($"Falconer: {falconers[RandomNumber(falconers)]}");
+            Console.WriteLine($"with Falcon: {falcons[RandomNumber(falcons)]}");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            
+            //End the program with blank line and instructions
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine("Press <Enter> to quit the program");
+            Console.ReadKey();
+        }
+
+        static int RandomNumber(string[] array)
+        {
+            var rand = new Random();
+            return rand.Next(1, array.Length);
+        }
+    }
+}
